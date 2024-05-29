3c791372f0e307552a68b3d135bd675b98d8b546
Deben21 <pundebendra7@gmail.com>
2024-05-29 16:59:15 +0545

    changes

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -24,12 +24,17 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/duck.jpg',
+    imageUrl: imageUrl,
     creator: { name: 'miccilian' }
   });
   post
